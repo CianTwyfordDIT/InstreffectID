e36da197b34753bfd12416ce9954a4c9f84ac03f
CianTwyfordDIT <C16380866@mydit.ie>
2020-03-22 12:14:06 +0000

    Configuration Class

---
+++ Configuration.py
@@ -0,0 +1,17 @@
+import os
+
+
+# Configuration class to initialise constants for neural network model to
+# avoid hard coding
+class Configuration:
+    # Number of triangular filters, number of MFCC features, length of signal to calculate
+    # the Fast Fourier Transform and the sampling rate
+    def __init__(self, mode='conv', nfilt=26, nfeat=13, nfft=512, rate=16000):
+        self.mode = mode
+        self.nfilt = nfilt
+        self.nfeat = nfeat
+        self.nfft = nfft
+        self.rate = rate
+        self.step = int(rate/10)  # Return 1/10 of a second
+        self.modelPath = os.path.join('SavedModel', mode+'.model')
+        self.picklePath = os.path.join('SavedPickle', mode+'.p')
