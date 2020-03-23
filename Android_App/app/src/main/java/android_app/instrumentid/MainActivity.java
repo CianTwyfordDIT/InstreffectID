314913b1cb54914d8bbee835ea6aaf5f4d143691
CianTwyfordDIT <C16380866@mydit.ie>
2020-03-23 21:01:05 +0000

    View Device Files And Get Path

--- Android_App/app/src/main/java/android_app/instrumentid/MainActivity.java
+++ Android_App/app/src/main/java/android_app/instrumentid/MainActivity.java
@@ -1,7 +1,11 @@
 package android_app.instrumentid;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -16,12 +20,30 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity
 {
 
+    TextView pathText;
+    Button uploadFile;
+    Intent fileIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pathText = (TextView) findViewById(R.id.pathText);
+        uploadFile = (Button) findViewById(R.id.uploadFile);
+
+        uploadFile.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                fileIntent.setType("*/*");
+                startActivityForResult(fileIntent, 10);
+
+            }
+        });
         serverConnection();
     }
 
@@ -90,5 +112,22 @@ public class MainActivity extends AppCompatActivity
                 });
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        switch (requestCode)
+        {
+            case 10:
+
+                if (resultCode == RESULT_OK)
+                {
+                    String path = data.getData().getPath();
+                    pathText.setText(path);
+                }
+
+                break;
+        }
     }
 }
