40b8e95c88859a805f5af1270886ae32e2ff7d97
CianTwyfordDIT <C16380866@mydit.ie>
2020-03-24 13:07:49 +0000

    Prediction Screen Initial Commit

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+
+# Create path variable
+path = 'C:/Users/ciant/OneDrive/Documents/Year4/FinalYearProject/InstrumentID/Predict_Audio/'
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def handle_request():
-    return "Connected to Flask Server :D"
+@app.route('/uploadFile', methods=['GET', 'POST'])
+def uploadFile():
+    if request.method == 'PUT':
+        file = request.files['file']
+        filePath = path+secure_filename(file.filename)
+        file.save(filePath)
+        response = "Success"
+
+        return response
 
 
 if __name__ == "__main__":
